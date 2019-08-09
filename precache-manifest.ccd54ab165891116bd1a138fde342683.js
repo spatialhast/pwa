21d9edd79c21fd7d9589c1b883dc34ca0279f0fe
Alex <kleshnin.alexander@gmail.com>
2019-08-09 11:41:58 +0300

    init

---
+++ precache-manifest.ccd54ab165891116bd1a138fde342683.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "fc3e1fe896032818973c",
+    "url": "/js/chunk-vendors.f5ea6abd.js"
+  },
+  {
+    "revision": "88bfe6376af250473a2d",
+    "url": "/js/app.6e61eeee.js"
+  },
+  {
+    "revision": "92f50dca9c8d7cabd838",
+    "url": "/js/about.646dca84.js"
+  },
+  {
+    "revision": "02a75e321721a4fd6c685cf8a29a89f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "88bfe6376af250473a2d",
+    "url": "/css/app.ed4a70a1.css"
+  }
+];
